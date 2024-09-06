16eed4d98bba8e878d4858ab6a6e1ee0822998e4
tanguy.ducrocq <tanguy.ducrocq@epita.fr>
2024-09-06 18:46:51 +0200

    fix(menu): corrected display and now the zoom is possible without weird things

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -2,9 +2,9 @@ import { FaInstagram, FaTiktok } from "react-icons/fa";
 
 export function BannerComponent() {
   return (
-    <div id="banner-slider" className="relative flex flex-row px-10 space-x-10 py-32 overflow-x-hidden">
+    <div id="banner-slider" className="relative flex flex-row justify-center py-32 overflow-x-hidden">
         {/* Background SVG */}
-        <div className="absolute inset-x-0 left-40 top-80 z-0 max-sm:hidden" style={{ height: '150px' }}>
+        <div className="absolute justify-center ml-96 mt-52 z-0 max-sm:hidden" style={{ height: '150px' }}>
           <div className="relative w-full h-full">
             <div className="absolute bottom-0 left-2/3 transform -translate-x-1/2 bg-gradient-custom rounded-custom rotate-[-40deg]">
               <svg data-depth="0.05" id="banner-main-svg" viewBox="0 0 220 100" xmlns="http://www.w3.org/2000/svg" className="w-[900px] h-[900px]">
@@ -20,10 +20,10 @@ export function BannerComponent() {
         </div>
 
       {/* Main Content */}
-      <div className="flex flex-row px-7 z-10">
+      <div className="flex flex-row z-10">
         
         {/* Social Media Links */}
-        <div className="flex flex-col space-y-10 py-44 max-sm:hidden">
+        <div className="flex flex-col space-y-10 py-44 max-sm:hidden mr-24">
           <a href="https://www.tiktok.com/@sterenova_" className="text-black hover:text-gray-500">
             <FaTiktok className="h-6 w-6" />
           </a>
@@ -33,18 +33,16 @@ export function BannerComponent() {
         </div>
 
         {/* Description */}
-        <div className="py-32 px-20 max-w-2xl">
+        <div className="mt-32">
           <h1 className="font-bold text-5xl">STERENOVA</h1>
-          <p className="py-10 text-black/70">
+          <p className="py-10 text-black/70 max-w-sm mr-40">
             Bienvenue chez Sterenova !
             <br />
             Nous vous proposons d'équiper vos soirées pour qu'elles soient mémorables.
             <br />
             Éclairage pour toutes sortes d'ambiance et matériel sonore sont à votre portée.
             <br />
-            N'hésitez pas à explorer notre site et découvrez notre large gamme de
-            <br />
-            produits.
+            N'hésitez pas à explorer notre site et découvrez notre large gamme de produits.
           </p>
           <a className="rounded-3xl bg-red-600/90 py-3 px-6 shadow-sm text-white hover:bg-gradient-to-r from-[rgba(98,37,181,0.8)] to-[rgba(128,33,181,0.8)]" href="#contact">
             Nous contacter
@@ -52,7 +50,7 @@ export function BannerComponent() {
         </div>
 
         {/* Image */}
-        <div className="flex-shrink-0 px-8 py-20">
+        <div className="px-8 py-20 mr-20">
           <div className="max-w-md">
             <img src="img/sterenova/3mans.png" alt="plant" />
           </div>
--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -16,23 +16,22 @@ export function NavBarComponent() {
   }, []);
 
   return (
-    <nav className={`${scrolled ? "bg-gradient-to-r from-[rgba(98,37,181,0.8)] to-[rgba(128,33,181,0.8)]" : "bg-transparent"} w-full fixed z-30 overflow-x-hidden`}>
-        {/* Navbar Content */}
-        <div className="flex justify-between items-center relative z-20 px-4 py-4 max-h-36">
-            {/* Navbar title/logo */}
-            <a href="#" className="">
-                <img src="public/img/sterenova/logo_25.png" alt="Logo"/>
-            </a>
-
-            {/* Menu for larger screens */}
-            <nav className="hidden md:flex space-x-8">
-                <a href="#" className="text-white hover:text-white hover:ring-4 transition">Accueil</a>
-                <a href="#portfolio-sec" className="text-white hover:ring-4 transition">Réalisations</a>
-                <a href="#gallery-sec" className="text-white hover:ring-4 transition">La Galerie</a>
-                <a href="#pricing-sec" className="text-white hover:ring-4 transition">Nos Packs</a>
-                <a href="#contact-sec" className="text-white hover:ring-4 transition">Nous Contacter</a>
-            </nav>
+    <nav className={`${scrolled ? "bg-gradient-to-r from-[rgba(98,37,181,0.8)] to-[rgba(128,33,181,0.8)]" : "bg-transparent"}
+                        w-full fixed z-30 overflow-x-hidden`}>
+      <div className="flex justify-center items-center relative z-20 px-4 py-4 max-h-36 w-full">
+        <div className="flex items-center justify-between w-full max-w-screen-xl px-4">
+          <a className="flex">
+            <img src="public/img/sterenova/logo_25.png" alt="Logo" />
+          </a>
+          <nav className="hidden md:flex space-x-8">
+            <a href="#" className="text-white hover:text-white hover:ring-4 transition">Accueil</a>
+            <a href="#portfolio-sec" className="text-white hover:ring-4 transition">Réalisations</a>
+            <a href="#gallery-sec" className="text-white hover:ring-4 transition">La Galerie</a>
+            <a href="#pricing-sec" className="text-white hover:ring-4 transition">Nos Packs</a>
+            <a href="#contact-sec" className="text-white hover:ring-4 transition">Nous Contacter</a>
+          </nav>
         </div>
+      </div>
     </nav>
   );
 }
